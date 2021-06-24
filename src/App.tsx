49ae5f7f38f1bfd7f12890bfb447cf997098fec2
Lucas340 <lucasstorres100@gmail.com>
2021-06-24 16:27:13 -0300

    🐛 | Fixing router mistake

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" component={Home} exact />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/rooms/new" component={NewRoom} exact />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
