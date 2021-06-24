e1b6ccf9ff5293740595b8bf3102c385123749bb
Lucas340 <lucasstorres100@gmail.com>
2021-06-24 15:15:42 -0300

    ✨ | Introducing react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import { AuthContextProvider } from './contexts/AuthContext';
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" component={Home} exact />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
