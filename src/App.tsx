9d1524cad5d2bf2f001d1b9368e4db11306172ea
Lucas340 <lucasstorres100@gmail.com>
2021-06-26 09:45:57 -0300

    ✨ | Introducing admin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" component={Home} exact />
           <Route path="/rooms/new" component={NewRoom} exact />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,76 @@
+import { useParams, useHistory } from 'react-router-dom';
+
+import { Question } from '../components/Question';
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import { database } from '../services/firebase';
+import { useRoom } from '../hooks/useRoom';
+
+import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
+
+import '../styles/room.scss';
+
+type ParamsProps = {
+  id: string;
+};
+
+export function AdminRoom() {
+  const { id } = useParams<ParamsProps>();
+  const { questions, title } = useRoom(id);
+  const history = useHistory();
+
+  async function handleDeleteQuestion(questionID: string) {
+    if (window.confirm('Você tem certeza que deseja deletar a pergunta?')) {
+      await database.ref(`rooms/${id}/questions/${questionID}`).remove();
+    }
+  }
+
+  async function handleEndRoom() {
+    await database.ref(`rooms/${id}`).update({
+      endedAt: new Date(),
+    });
+
+    history.push('/');
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>
+            <RoomCode code={id} />
+            <Button isOutlined onClick={handleEndRoom}>
+              Encerrar sala
+            </Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <div className="question-list">
+          {questions.map((question) => (
+            <Question
+              key={question.id}
+              content={question.content}
+              author={question.author}
+            >
+              <button
+                type="button"
+                onClick={() => handleDeleteQuestion(question.id)}
+              >
+                <img src={deleteImg} alt="Remover pergunta" />
+              </button>
+            </Question>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
